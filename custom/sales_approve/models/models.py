9db1441f657a29953cf24ffad4d046d61fc7fc74
jay suthar <jaysuthar743@gmail.com>
2021-03-27 19:03:18 +0530

    sale approve, guest, task done

--- custom/sales_approve/models/models.py
+++ custom/sales_approve/models/models.py
@@ -6,12 +6,11 @@ from odoo import models, fields, api
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    sales_limit = fields.Float(string='Sales Limit')
+    sales_limit = fields.Float(string='Sales Limit', config_parameter="sales_approve.sales_limit")
 
     @api.model
     def get_values(self):
         res = super(ResConfigSettings, self).get_values()
-
         res['sales_limit'] = (self.env['ir.config_parameter'].sudo().get_param('sales_approve.sales_limit',
                                                                                default=0))
         return res
@@ -25,7 +24,15 @@ class ResConfigSettings(models.TransientModel):
 class SalesSelectionAdd(models.Model):
     _inherit = "sale.order"
 
-    state = fields.Selection(selection_add=[('to_approve', 'To approve'), ('sale', 'Sale Order')])
+    state = fields.Selection(selection_add=[('to_approve', 'To Approve'), ('sale', 'Sale Order')])
 
     def action_to_approve(self):
-        pass
+        self.state = 'sale'
+
+    def action_confirm(self):
+        res = super(SalesSelectionAdd, self).action_confirm()
+        sales_limit = self.env['ir.config_parameter'].sudo().get_param('sales_approve.sales_limit') or False
+        if sales_limit:
+            if float(self.amount_total) > float(sales_limit):
+                self.state = 'to_approve'
+        return res
